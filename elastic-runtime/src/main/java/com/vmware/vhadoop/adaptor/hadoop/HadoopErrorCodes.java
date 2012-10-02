1feaf9142c9430d179174ec742466d9baaad4ad3
Kinshuk Govil <kinshuk@vmware.com>
2012-10-02 14:42:35 -0700

    Add copyright

--- elastic-runtime/src/main/java/com/vmware/vhadoop/adaptor/hadoop/HadoopErrorCodes.java
+++ elastic-runtime/src/main/java/com/vmware/vhadoop/adaptor/hadoop/HadoopErrorCodes.java
@@ -1,3 +1,18 @@
+/***************************************************************************
+ * Copyright (c) 2012 VMware, Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
 package com.vmware.vhadoop.adaptor.hadoop;
 
 import static com.vmware.vhadoop.adaptor.hadoop.HadoopErrorCodes.ParamTypes.COMMAND;
@@ -15,8 +30,6 @@ import java.util.logging.Logger;
 /**
  * Error codes here represent values which can be returned from execution of remote scripts
  * 
- * @author bcorrie
- *
  */
 public class HadoopErrorCodes {
    private Map<Integer, ErrorCode> _errorCodes;
