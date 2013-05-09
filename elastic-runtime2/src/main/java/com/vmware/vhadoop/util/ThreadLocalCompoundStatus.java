b0fd9c0f53172aa51f9c9aa99121900ecda38848
ghicken <ghicken@vmware.com>
2013-05-08 17:04:14 -0700

    Corrects mistaken assignment instead of test

--- elastic-runtime2/src/main/java/com/vmware/vhadoop/util/ThreadLocalCompoundStatus.java
+++ elastic-runtime2/src/main/java/com/vmware/vhadoop/util/ThreadLocalCompoundStatus.java
@@ -8,21 +8,21 @@ package com.vmware.vhadoop.util;
  * As further protection against memory leaks, the capability has been added for the ThreadLocalCompoundStatus
  *   to just return a dummy object unless the current thread explicitly called initialize(). That calling thread
  *   is then responsible for explicitly calling remove() once the call stack has returned. The other advantage
- *   of this is that code using the CompoundStatus does not need to be full of null checks. It will either get 
+ *   of this is that code using the CompoundStatus does not need to be full of null checks. It will either get
  *   a useless dummy or something useful.
  */
 public class ThreadLocalCompoundStatus extends ThreadLocal<CompoundStatus> {
    class InitializedState {
       boolean _initialized;
    }
-   
+
    ThreadLocal<InitializedState> _initializedState = new ThreadLocal<InitializedState>() {
       @Override
       public InitializedState get() {
          return new InitializedState();
       }
    };
-   
+
    /* Ensures that only a single caller can initialize the ThreadLocal which is the same caller that removes it
     * This means that any code can call get() and if it's running in a thread that hasn't initialized a CompoundStatus
     *   the code gets a dummy CompoundStatus to avoid a NPE, but a memory leak cannot occur */
@@ -30,7 +30,7 @@ public class ThreadLocalCompoundStatus extends ThreadLocal<CompoundStatus> {
       _initializedState.get()._initialized = true;
       return super.get();     /* Returns the result of initialValue() and then that same object, until remove() is called */
    }
-   
+
    @Override
    protected CompoundStatus initialValue() {
       /* Return the root CompoundStatus for a thread operation. Any number of compound statuses can be added to this one */
@@ -41,14 +41,14 @@ public class ThreadLocalCompoundStatus extends ThreadLocal<CompoundStatus> {
    public CompoundStatus get() {
       /* If this thread has not explicitly initialized its TLCS, it should not be allowed to create a ThreadLocal
        * Instead it gets a useless dummy */
-      if (_initializedState.get()._initialized = false) {
+      if (_initializedState.get()._initialized == false) {
          _initializedState.remove();
          return new CompoundStatus("DUMMY_STATUS");
       }
       return super.get();
    }
-   
-   @Override 
+
+   @Override
    public void remove() {
       /* Any thread that has called initialize(), must follow it up with a call to remove() once the operation is completed */
       super.remove();
