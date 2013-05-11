298658a788178af5929da154221c9bbdf3ff30ee
Ben Corrie <bcorrie@office1-sfo2-dhcp206.eng.vmware.com>
2013-05-10 20:03:47 -0700

    Augment vhm integration testing

--- elastic-runtime2/src/test/java/com/vmware/vhadoop/vhm/TrivialScaleStrategy.java
+++ elastic-runtime2/src/test/java/com/vmware/vhadoop/vhm/TrivialScaleStrategy.java
@@ -1,31 +1,62 @@
 package com.vmware.vhadoop.vhm;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-import com.vmware.vhadoop.api.vhm.ClusterMap;
-import com.vmware.vhadoop.api.vhm.ClusterMapReader;
+import com.vmware.vhadoop.api.vhm.events.ClusterScaleCompletionEvent;
 import com.vmware.vhadoop.api.vhm.events.ClusterScaleEvent;
 import com.vmware.vhadoop.api.vhm.strategy.ScaleStrategy;
 import com.vmware.vhadoop.api.vhm.strategy.ScaleStrategyContext;
+import com.vmware.vhadoop.vhm.events.ClusterScaleDecision;
 
-public class TrivialScaleStrategy implements ScaleStrategy {
+public class TrivialScaleStrategy extends AbstractClusterMapReader implements ScaleStrategy {
    String _testKey;
+   Map<String, ClusterScaleOperation> _clusterScaleOperations;
+   TrivialClusterScaleOperation _trivialClusterScaleOperation;
+   
+   public class TrivialClusterScaleOperation extends ClusterScaleOperation {
+      String _clusterId;
+      Set<ClusterScaleEvent> _events;
+      ScaleStrategyContext _context;
+      
+      public TrivialClusterScaleOperation() {
+         initialize(TrivialScaleStrategy.this);
+      }
+      
+      void setClusterId(String clusterId) {
+         _clusterId = clusterId;
+      }
+      
+      void setEvents(Set<ClusterScaleEvent> events) {
+         _events = events;
+      }
+      
+      void setContext(ScaleStrategyContext context) {
+         _context = context;
+      }
+      
+      public String getClusterId() {
+         return _clusterId;
+      }
+      
+      public Set<ClusterScaleEvent> getEvents() {
+         return _events;
+      }
+      
+      public ScaleStrategyContext getContext() {
+         return _context;
+      }
+
+      @Override
+      public ClusterScaleCompletionEvent localCall() throws Exception {
+         return new ClusterScaleDecision(_clusterId);
+      }
+   }
    
    public TrivialScaleStrategy(String testKey) {
       _testKey = testKey;
-   }
-
-   @Override
-   public void initialize(ClusterMapReader parent) {
-   }
-
-   @Override
-   public ClusterMap getAndReadLockClusterMap() {
-      return null;
-   }
-
-   @Override
-   public void unlockClusterMap(ClusterMap clusterMap) {
+      _clusterScaleOperations = new HashMap<String, ClusterScaleOperation>();
    }
 
    @Override
@@ -33,15 +64,41 @@ public class TrivialScaleStrategy implements ScaleStrategy {
       return _testKey;
    }
 
+   /* If it's not static, it can't be created reflectively */
+   public static class TrivialStrategyContext implements ScaleStrategyContext {
+      private String _someContextInfo;
+
+      public String getSomeContextInfo() {
+         return _someContextInfo;
+      }
+
+      public void setSomeContextInfo(String someContextInfo) {
+         _someContextInfo = someContextInfo;
+      }
+   }
+   
    @Override
    public Class<? extends ScaleStrategyContext> getStrategyContextType() {
-      return null;
+      return TrivialStrategyContext.class;
    }
 
+   void setClusterScaleOperation(String clusterId, ClusterScaleOperation cso) {
+      _clusterScaleOperations.put(clusterId, cso);
+   }
+   
    @Override
    public ClusterScaleOperation getClusterScaleOperation(String clusterId,
          Set<ClusterScaleEvent> events, ScaleStrategyContext context) {
-      return null;
+      ClusterScaleOperation returnVal = _clusterScaleOperations.get(clusterId);
+      if (returnVal == null) {
+         returnVal = new TrivialClusterScaleOperation();
+      }
+      if (returnVal instanceof TrivialClusterScaleOperation) {
+         ((TrivialClusterScaleOperation)returnVal).setClusterId(clusterId);
+         ((TrivialClusterScaleOperation)returnVal).setContext(context);
+         ((TrivialClusterScaleOperation)returnVal).setEvents(events);
+      }
+      return returnVal;
    }
 
 }
--- elastic-runtime2/src/test/java/com/vmware/vhadoop/vhm/VHMIntegrationTest.java
+++ elastic-runtime2/src/test/java/com/vmware/vhadoop/vhm/VHMIntegrationTest.java
@@ -8,17 +8,22 @@ import static org.junit.Assert.*;
 import com.vmware.vhadoop.api.vhm.ClusterMap;
 import com.vmware.vhadoop.api.vhm.ClusterMapReader;
 import com.vmware.vhadoop.api.vhm.ClusterMap.ExtraInfoToScaleStrategyMapper;
+import com.vmware.vhadoop.api.vhm.events.ClusterScaleCompletionEvent;
 import com.vmware.vhadoop.api.vhm.events.EventConsumer;
 import com.vmware.vhadoop.api.vhm.events.EventProducer;
 import com.vmware.vhadoop.api.vhm.events.ClusterStateChangeEvent.VMEventData;
 import com.vmware.vhadoop.api.vhm.strategy.ScaleStrategy;
-import com.vmware.vhadoop.vhm.strategy.ManualScaleStrategy;
+import com.vmware.vhadoop.util.ThreadLocalCompoundStatus;
+import com.vmware.vhadoop.vhm.TrivialScaleStrategy.TrivialClusterScaleOperation;
 
 public class VHMIntegrationTest extends AbstractJUnitTest implements EventProducer, ClusterMapReader {
    VHM _vhm;
    StandaloneSimpleVCActions _vcActions;
    ExtraInfoToScaleStrategyMapper _strategyMapper;
    ClusterStateChangeListenerImpl _clusterStateChangeListener;
+   TrivialScaleStrategy _trivialScaleStrategy;
+   
+   public static final String STRATEGY_KEY = "StrategyKey";
    
    /* EventProducer fields */
    EventConsumer _eventConsumer;
@@ -47,10 +52,11 @@ public class VHMIntegrationTest extends AbstractJUnitTest implements EventProduc
       _strategyMapper = new ExtraInfoToScaleStrategyMapper() {
          @Override
          public String getStrategyKey(VMEventData vmd) {
-            return ManualScaleStrategy.MANUAL_SCALE_STRATEGY_KEY;
+            return STRATEGY_KEY;
          }
       };
-      _vhm = new VHM(_vcActions, new ScaleStrategy[]{new TrivialScaleStrategy("fooKey")}, _strategyMapper, null);
+      _trivialScaleStrategy = new TrivialScaleStrategy(STRATEGY_KEY);
+      _vhm = new VHM(_vcActions, new ScaleStrategy[]{_trivialScaleStrategy}, _strategyMapper, new ThreadLocalCompoundStatus());
       _vhm.registerEventProducer(_clusterStateChangeListener);
       _vhm.registerEventProducer(this);
       _vhm.start();
@@ -58,33 +64,92 @@ public class VHMIntegrationTest extends AbstractJUnitTest implements EventProduc
 
    @After
    public void cleanup() {
+      try {
+         Thread.sleep(1000);
+      } catch (InterruptedException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
       _vhm.stop(true);
    }
    
-   @Test
-   public void sanityTest() {
-      int numClusters = 3;
-      
-      populateSimpleClusterMap(numClusters, 4, false);    /* Blocks until CSCL has generated all events */
+   /* After updating VHM via the CSCL, wait for the updates to appear in ClusterMap */
+   private boolean waitForTargetClusterCount(int clusterCount, long timeoutMillis) {
       String[] clusterIds = null;
-      int maxIterations = 100;
-      
+      long pollTimeMillis = 10;
+      long maxIterations = timeoutMillis / pollTimeMillis;
       /* VHM may still be in the process of dealing with the events coming from the CSCL, so need to wait for the expected value */
-      while ((clusterIds == null) || (clusterIds.length != 3)) {
+      while ((clusterIds == null) || (clusterIds.length != clusterCount)) {
          ClusterMap clusterMap = getAndReadLockClusterMap();
          assertNotNull(clusterMap);
          clusterIds = clusterMap.getAllKnownClusterIds();
          unlockClusterMap(clusterMap);
          try {
-            Thread.sleep(10);
+            Thread.sleep(pollTimeMillis);
          } catch (InterruptedException e) {
             e.printStackTrace();
          }
-         if (--maxIterations <= 0) break;
+         if (--maxIterations <= 0) return false;
       }
+      return true;
+    }
+
+   private boolean waitForClusterScaleCompletionEvent(String clusterId, int timeoutMillis) {
+      ClusterScaleCompletionEvent toWaitFor = null;
+      long pollTimeMillis = 10;
+      long maxIterations = timeoutMillis / pollTimeMillis;
+      /* VHM may still be in the process of dealing with the events coming from the CSCL, so need to wait for the expected value */
+      while (toWaitFor == null) {
+         ClusterMap clusterMap = getAndReadLockClusterMap();
+         assertNotNull(clusterMap);
+         toWaitFor = clusterMap.getLastClusterScaleCompletionEvent(clusterId);
+         unlockClusterMap(clusterMap);
+         try {
+            Thread.sleep(pollTimeMillis);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+         if (--maxIterations <= 0) return false;
+      }
+      return true;
+   }
+
+   @Test
+   public void sanityTest() {
+      int numClusters = 3;
+      populateSimpleClusterMap(numClusters, 4, false);    /* Blocks until CSCL has generated all events */
+      assertTrue(waitForTargetClusterCount(3, 1000));
+
+      ClusterMap clusterMap = getAndReadLockClusterMap();
+      assertNotNull(clusterMap);
+      String[] clusterIds = clusterMap.getAllKnownClusterIds();
+      unlockClusterMap(clusterMap);
       
+      /* Verify that the number of clusters seen by vcActions is reflected in the ClusterMap */
       assertNotNull(clusterIds);
       assertEquals(numClusters, clusterIds.length);
+   }
+
+   @Test
+   public void testInvokeScaleStrategy() {
+      int numClusters = 3;
+      populateSimpleClusterMap(numClusters, 4, false);    /* Blocks until CSCL has generated all events */
+      assertTrue(waitForTargetClusterCount(3, 1000));
+      
+      String clusterId = deriveClusterIdFromClusterName(_clusterNames.iterator().next());
+      /* Create a ClusterScaleOperation, which controls how a cluster is scaled */
+      TrivialClusterScaleOperation tcso = _trivialScaleStrategy.new TrivialClusterScaleOperation();
+      /* Add the test ClusterScaleOperation to the test scale strategy */
+      _trivialScaleStrategy.setClusterScaleOperation(clusterId, tcso);
+
+      /* Simulate a cluster scale event being triggered from an EventProducer */
+      _eventConsumer.placeEventOnQueue(new TrivialClusterScaleEvent(null, null, clusterId));
+      /* Wait for VHM to respond, having invoked the ScaleStrategy */
+      assertTrue(waitForClusterScaleCompletionEvent(clusterId, 1000));
+      
+      assertEquals(clusterId, tcso.getClusterId());
+      assertNotNull(tcso.getContext());
+      assertNotNull(tcso.getThreadLocalCompoundStatus());
    }
 
    @Override
