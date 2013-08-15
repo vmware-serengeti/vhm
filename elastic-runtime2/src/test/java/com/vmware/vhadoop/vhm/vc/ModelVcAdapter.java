2fbcf8ef867c4d72a7312bf05ae5bb3f360817dc
Ben Corrie <bcorrie@bcorrie-mbp.local>
2013-08-15 03:53:50 -0700

    Trivial: Add OSS headers to all source files where it's missing

--- elastic-runtime2/src/test/java/com/vmware/vhadoop/vhm/vc/ModelVcAdapter.java
+++ elastic-runtime2/src/test/java/com/vmware/vhadoop/vhm/vc/ModelVcAdapter.java
@@ -12,6 +12,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 ***************************************************************************/
+
 package com.vmware.vhadoop.vhm.vc;
 
 import java.util.ArrayList;
