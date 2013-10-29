4cea88c9019772ce8f8bf78276971a3872592a5b
ghicken <ghicken@vmware.com>
2013-10-29 14:37:32 -0700

    S31830: adds skeletal outline of unit tests for ssh connection cache

---
+++ elastic-runtime2/src/test/java/com/vmware/vhadoop/vhm/hadoop/SshConnectionCacheTest.java
@@ -0,0 +1,44 @@
+package com.vmware.vhadoop.vhm.hadoop;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SshConnectionCacheTest
+{
+
+   @Ignore
+   @Test
+   public void cachedConnectionIsReused() {
+
+   }
+
+   @Ignore
+   @Test
+   public void leastRecentlyUsedIsEvicted() {
+
+   }
+
+   @Ignore
+   @Test
+   public void evictedConnectionIsRecreated() {
+
+   }
+
+   @Ignore
+   @Test
+   public void previouslyEvictedCachedConnectionIsReused() {
+
+   }
+
+   @Ignore
+   @Test
+   public void evictedConnectionDoesntCloseWhileInUse() {
+
+   }
+
+   @Ignore
+   @Test
+   public void cachedDroppedConnectionIsRecreated() {
+
+   }
+}
