ecda8d119fb3379966185163421cfcd0ddce1a40
ghicken <ghicken@vmware.com>
2013-11-05 10:12:04 -0800

    S31441: interim updates to VcVlsi code to support min/max. Only as much as necessary to allow a model implementation

--- elastic-runtime2/src/main/java/com/vmware/vhadoop/api/vhm/VCActions.java
+++ elastic-runtime2/src/main/java/com/vmware/vhadoop/api/vhm/VCActions.java
@@ -35,11 +35,12 @@ public interface VCActions {
    public class MasterVmEventData {
       public Boolean _enableAutomation;
       public Integer _minInstances;
+      public String _instanceRange;
       public Integer _jobTrackerPort;
 
       @Override
       public String toString() {
-         return "enableAutomation="+_enableAutomation+", minInstances="+_minInstances+", jobTrackerPort="+_jobTrackerPort;
+         return "enableAutomation="+_enableAutomation+", minInstances="+_minInstances+", instanceRange="+_instanceRange+", jobTrackerPort="+_jobTrackerPort;
       }
    }
 
--- elastic-runtime2/src/main/java/com/vmware/vhadoop/vhm/vc/VcVlsiHelper.java
+++ elastic-runtime2/src/main/java/com/vmware/vhadoop/vhm/vc/VcVlsiHelper.java
@@ -16,7 +16,7 @@
 package com.vmware.vhadoop.vhm.vc;
 
 import static com.vmware.vhadoop.vhm.vc.VcVlsi.VHM_EXTRA_CONFIG_AUTOMATION_ENABLE;
-import static com.vmware.vhadoop.vhm.vc.VcVlsi.VHM_EXTRA_CONFIG_AUTOMATION_MIN_INSTANCES;
+import static com.vmware.vhadoop.vhm.vc.VcVlsi.VHM_EXTRA_CONFIG_AUTOMATION_INSTANCE_RANGE;
 import static com.vmware.vhadoop.vhm.vc.VcVlsi.VHM_EXTRA_CONFIG_ELASTIC;
 import static com.vmware.vhadoop.vhm.vc.VcVlsi.VHM_EXTRA_CONFIG_JOB_TRACKER_PORT;
 import static com.vmware.vhadoop.vhm.vc.VcVlsi.VHM_EXTRA_CONFIG_MASTER_MOREF;
@@ -51,8 +51,8 @@ public class VcVlsiHelper {
             vmData._isElastic = value.equalsIgnoreCase("true");
          } else if (key.equals(VHM_EXTRA_CONFIG_AUTOMATION_ENABLE)) {
             getMasterVmData(vmData)._enableAutomation = value.equalsIgnoreCase("true");
-         } else if (key.equals(VHM_EXTRA_CONFIG_AUTOMATION_MIN_INSTANCES)) {
-            getMasterVmData(vmData)._minInstances = Integer.valueOf(value);
+         } else if (key.equals(VHM_EXTRA_CONFIG_AUTOMATION_INSTANCE_RANGE)) {
+            getMasterVmData(vmData)._instanceRange = value;
          } else if (key.equals(VHM_EXTRA_CONFIG_JOB_TRACKER_PORT)) {
             getMasterVmData(vmData)._jobTrackerPort = Integer.valueOf(value);
          }
